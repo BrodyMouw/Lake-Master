6b670ab89f366c79fa5c7d67113843b9a97a61f3
Brody School Account <brody.mouw@jacks.sdstate.edu>
2020-10-24 23:12:49 -0500

    cart page done

---
+++ LakeLife/LakeLife/Helpers/CalculateTotal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LakeLife.Helpers
+{
+    public static class CalculateTotal
+    {
+        public static double calculateTotal()
+        {
+            return Calculate();
+        }
+
+        public static double Calculate()
+        {
+            double price = 0;
+
+            if (Settings.ItemStatus1)
+                price += 60.0;
+            if (Settings.ItemStatus2)
+                price += 300.0;
+            if (Settings.ItemStatus3)
+                price += 900;
+            if (Settings.ItemStatus4)
+                price += 0;
+
+            return price;
+        }
+    }
+}
---
+++ LakeLife/LakeLife/Helpers/CartCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LakeLife.Helpers
+{
+    public static class CartCounter
+    {
+        public static int CartCount()
+        {
+            return Calculate();
+        }
+
+        public static int Calculate()
+        {
+            int count = 0;
+
+            if (Settings.ItemStatus1)
+                count = count + 1;
+            if (Settings.ItemStatus2)
+                count = count + 1;
+            if (Settings.ItemStatus3)
+                count = count + 1;
+            if (Settings.ItemStatus4)
+                count = count + 1;
+
+            return count;
+        }
+    }
+}
